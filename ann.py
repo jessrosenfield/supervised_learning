d67af135ce72ff8b9c5ef1f8b8ed2ada8d689d8c
jessrosenfield <rosenfield.jess@gmail.com>
2016-02-03 03:43:56 +0000

    Run bc_ann

--- ann.py
+++ ann.py
@@ -14,17 +14,20 @@ bc_nn = Classifier(
         Layer("Softmax")])
 num_iterations = np.arange(500, 20250, 250)
 param_grid = {'n_iter': num_iterations}
-clf = GridSearchCV(bc_nn, param_grid, n_jobs=-1, cv=10, verbose=2)
+clf = GridSearchCV(bc_nn, param_grid, n_jobs=-1, cv=10)
 clf.fit(bc_data, bc_target)
 print "Best estimatore: ", clf.best_estimator_
 print "Best parameters set found: ", clf.best_params_
-print classification_report(clf.grid_scores_)
-
+params = []
+error = []
+for gs in clf.grid_scores_:
+    params.append(gs[0]['n_iter'])
+    error.append(1 - gs[1])
+    print gs[0]['n_iter'], ":", gs[1]
 fig, ax = plt.subplots()
 ax.scatter(num_iterations, mean_errors)
-ax.plot([0, num_iterations.max()], [0, mean_errors.max()], 'k--', lw=4)
-ax.set_xlabel('')
-ax.set_ylabel('Predicted')
-plt.savefig('ann-crossval-results.png')
-plt.show()
+ax.plot([0, max(params)], [0, max(error)], 'k--', lw=4)
+ax.set_xlabel('Iterations')
+ax.set_ylabel('Error')
+plt.savefig('ann-crossval-results2.png')
 
